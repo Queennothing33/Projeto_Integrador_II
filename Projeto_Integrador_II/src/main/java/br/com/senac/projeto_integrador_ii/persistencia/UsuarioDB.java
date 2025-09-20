8b166efd8fe58ac4e880b181e5a085608038748b
Queennothing33 <gparejas23@outlook.com>
2025-09-20 20:58:43 -0300

    Refatoração (UsuarioDB)

--- Projeto_Integrador_II/src/main/java/br/com/senac/projeto_integrador_ii/persistencia/UsuarioDB.java
+++ Projeto_Integrador_II/src/main/java/br/com/senac/projeto_integrador_ii/persistencia/UsuarioDB.java
@@ -1,34 +1,23 @@
 package br.com.senac.projeto_integrador_ii.persistencia;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
 
 public class UsuarioDB {
-        public static Usuario validarUsuarioSeguro(Usuario usuario) {
-        String sql = "SELECT * FROM usuario WHERE login = ? AND senha = ?";
-        Usuario usuarioEncontrado = null;
-                      
+
+    public Usuario validarUsuario(Usuario usuario) {
+        EntityManager em = JPAUtil.getEntityManager();
         try {
-        Connection conexao = DriverManager.getConnection("jdbc:mysql://localhost:3306/projeto_integrador_db", "root", "Het1981!");
-        PreparedStatement statement = conexao.prepareStatement(sql);
-                                  
-        statement.setString(1, usuario.getLogin());
-        statement.setString(2, usuario.getSenha());
-        ResultSet rs = statement.executeQuery();
-                      
-        while (rs.next()) {
-            usuarioEncontrado = new Usuario();
-            usuarioEncontrado.setId(rs.getInt("id"));
-            usuarioEncontrado.setLogin(rs.getString("login"));
-            usuarioEncontrado.setSenha(rs.getString("senha"));
-            usuarioEncontrado.setTipo(rs.getString("tipo"));
-                        }
-            } catch (SQLException ex) {
-                System.out.println("Sintaxe de comando invalida");
-             }                 
-            return usuarioEncontrado;
-             }  
+            TypedQuery<Usuario> query = em.createQuery(
+                    "SELECT u FROM Usuario u WHERE u.login = :login AND u.senha = :senha", Usuario.class);
+            query.setParameter("login", usuario.getLogin());
+            query.setParameter("senha", usuario.getSenha()); 
+            return query.getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        } finally {
+            em.close();
+        }
+    }
 }
