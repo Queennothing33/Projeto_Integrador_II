5ddd7659675f4ed74ef67bc859837ffc6906d9e5
Queennothing33 <gparejas23@outlook.com>
2025-09-20 14:26:33 -0300

    Refatoração (Musica)

--- Projeto_Integrador_II/src/main/java/br/com/senac/projeto_integrador_ii/persistencia/Musica.java
+++ Projeto_Integrador_II/src/main/java/br/com/senac/projeto_integrador_ii/persistencia/Musica.java
@@ -1,26 +1,51 @@
 package br.com.senac.projeto_integrador_ii.persistencia;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Musica {
+
     @Id
-    private int id;
-    
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // ID gerado automaticamente
+    private Long id;
+
     private String titulo;
     private String banda;
     private String tom;
     private String genero;
-    private String semestre_iniciado;
+    private String semestreIniciado;
     private String url;
 
-    public int getId() {
+    // Construtor vazio exigido pelo JPA
+    public Musica() {
+    }
+
+    public Musica(String titulo, String banda, String tom, String genero, String semestreIniciado, String url) {
+        this.titulo = titulo;
+        this.banda = banda;
+        this.tom = tom;
+        this.genero = genero;
+        this.semestreIniciado = semestreIniciado;
+        this.url = url;
+    }
+
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
+    }
+
+    public String getSemestreIniciado() {
+        return semestreIniciado;
+    }
+
+    public void setSemestreIniciado(String semestreIniciado) {
+        this.semestreIniciado = semestreIniciado;
     }
 
     public String getTitulo() {
@@ -55,19 +80,16 @@ public class Musica {
         this.genero = genero;
     }
 
-    public String getSemestre_iniciado() {
-        return semestre_iniciado;
-    }
-
-    public void setSemestre_iniciado(String semestre_iniciado) {
-        this.semestre_iniciado = semestre_iniciado;
-    }
-
     public String getUrl() {
         return url;
     }
 
     public void setUrl(String url) {
         this.url = url;
-    } 
+    }
+
+    public boolean isDoGenero(String generoBuscado) {
+        return this.genero != null && this.genero.equalsIgnoreCase(generoBuscado);
+    }
+    
 }
