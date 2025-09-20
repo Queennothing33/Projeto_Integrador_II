906d755aacb4dd0e131e6b0622e9723cb3402d8f
Queennothing33 <gparejas23@outlook.com>
2025-09-20 12:55:05 -0300

    Refatoração (JPAUtil)

--- Projeto_Integrador_II/src/main/java/br/com/senac/projeto_integrador_ii/persistencia/JPAUtil.java
+++ Projeto_Integrador_II/src/main/java/br/com/senac/projeto_integrador_ii/persistencia/JPAUtil.java
@@ -7,23 +7,23 @@ import jakarta.persistence.Persistence;
 public class JPAUtil {
     private static final String PERSISTENCE_UNIT = "ProjetoIntegrador-PU";
     
-    private static EntityManager em;
-    
     private static EntityManagerFactory fabrica;
     
-    public static EntityManager getEntityManager(){
-        if(fabrica == null || !fabrica.isOpen())
+    // Responsável apenas por fornecer a fábrica
+    private static EntityManagerFactory getEntityManagerFactory() {
+        if (fabrica == null || !fabrica.isOpen()) {
             fabrica = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
-        
-        if(em == null || !em.isOpen()) 
-            em = fabrica.createEntityManager();
-
-        return em;
+        }
+        return fabrica;
     }
     
-    public static void closeEtityManager(){
-        if(em.isOpen() && em != null){
-            em.close();
+    // Responsável por criar um novo EntityManager sob demanda
+    public static EntityManager getEntityManager() {
+        return getEntityManagerFactory().createEntityManager();
+    }
+
+    public static void closeFactory() {
+        if (fabrica != null && fabrica.isOpen()) {
             fabrica.close();
         }
     }
